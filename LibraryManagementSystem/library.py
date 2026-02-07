811b882151719a9876fc0281b2c097aa000371bc
swetha1507-s <swetha15072006@gmail.com>
2026-02-07 19:01:58 +0530

    Case Study 1: Initial Library Management System

---
+++ LibraryManagementSystem/library.py
@@ -0,0 +1,18 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, name):
+        self.books.append(name)
+        print(name, "added")
+
+    def show_books(self):
+        print("Books:")
+        for b in self.books:
+            print(b)
+
+
+lib = Library()
+lib.add_book("Python")
+lib.add_book("AI")
+lib.show_books()
