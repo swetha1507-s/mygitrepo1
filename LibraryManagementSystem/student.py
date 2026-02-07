679e338d5f516319bc2e7a156bc525da853e4395
swetha1507-s <swetha15072006@gmail.com>
2026-02-07 19:18:00 +0530

    Case Study 2: Added student registration module

---
+++ LibraryManagementSystem/student.py
@@ -0,0 +1,15 @@
+students = []
+
+def add_student(name, reg):
+    students.append((name, reg))
+    print("Added:", name)
+
+def show_students():
+    print("Students:")
+    for s in students:
+        print(s)
+
+
+add_student("Ravi", 101)
+add_student("Anu", 102)
+show_students()
